19c1dfcce9bb494e3a799f5307477225998f8b3e
Sally Ammous <70090183+sallytareq@users.noreply.github.com>
2021-02-07 09:23:37 +0200

    Firebase for Presentation

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYRExjLb9EZqlr-OHkBXpbKWPLrjf-vKU",
-  authDomain: "backup3-cc72b.firebaseapp.com",
-  projectId: "backup3-cc72b",
-  storageBucket: "backup3-cc72b.appspot.com",
-  messagingSenderId: "87944667159",
-  appId: "1:87944667159:web:9c6bf7c5174126f1ecbc52",
-  measurementId: "G-CGQPGM0GLQ"
+  apiKey: "AIzaSyDoVvhdN7KxKH2hVkZlEBUJ8l_aIaPUeCo",
+  authDomain: "chatizone-backend.firebaseapp.com",
+  databaseURL: "https://chatizone-backend-default-rtdb.firebaseio.com",
+  projectId: "chatizone-backend",
+  storageBucket: "chatizone-backend.appspot.com",
+  messagingSenderId: "48748900817",
+  appId: "1:48748900817:web:e4b4133c8ec6150396f0f0",
+  measurementId: "G-CTDT2YMVQ0"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
